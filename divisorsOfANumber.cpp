3dd40151a1922c8763094c058be515fbc3281218
Avinash <avinash31803@gmail.com>
2023-06-28 19:07:33 +0530

    added fiels

--- divisorsOfANumber.cpp
+++ divisorsOfANumber.cpp
@@ -4,6 +4,21 @@ using namespace std;
 
 // } Driver Code Ends
 //User function Template for C++
+
+// i*i<n can be used to prevent the calling of another function
+
+// vector<int> ls;
+// for(int i=0;i<sqrt(n);i++){            ----> O(sqrt(n))
+//     if((n%i)==0){
+//         ls.push_back(i);
+//         if(n/i!=i)
+//             ls.push_back(n/i);
+//     }
+//     sort(ls.begin(),ls.end()) ---> O(no of factors * log(no of factors))
+
+//  for(auto it : ls){ cout<<it<<endl;} --> O(no of factors)
+// }
+
 class Solution
 {
 public:
